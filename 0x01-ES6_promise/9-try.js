1e6594d035b6caaab8ff3f673ac7d868e820704a
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-23 22:25:49 +0000

    eslint task 10 error fixed

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,18 +1,18 @@
 // Queue push
 function guardrail(mathFunction) {
-    const queue = [];
-    let result;
-  
-    try {
-      result = mathFunction();
-    } catch (e) {
-      result = e.toString();
-    }
-  
-    queue.push(result);
-    queue.push('Guardrail was processed');
-  
-    return queue;
+  const queue = [];
+  let result;
+
+  try {
+    result = mathFunction();
+  } catch (e) {
+    result = e.toString();
   }
-  
-  export default guardrail;
+
+  queue.push(result);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
+
+export default guardrail;
