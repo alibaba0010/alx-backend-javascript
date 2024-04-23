531ec7ccb868ad24a5232830f3ffe2c0c85aa2f7
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-23 17:50:25 +0000

    commit fixed eslint error task 1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,14 @@
 // Only make Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      /* eslint-disable */
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
         if (true) {
           resolve();
         } else {
           reject();
         }
         /* eslint-enable */
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+  });
+}
+
+export default getResponseFromAPI;
