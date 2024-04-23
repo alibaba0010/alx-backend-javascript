fa9ac8a4508dc27d30505b6c1f40e4771907feb5
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-23 17:47:26 +0000

    commit files for es6 promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,15 @@
+// Only make Promise
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      /* eslint-disable */
+        if (true) {
+          resolve();
+        } else {
+          reject();
+        }
+        /* eslint-enable */
+    });
+  }
+  
+  export default getResponseFromAPI;
+  
