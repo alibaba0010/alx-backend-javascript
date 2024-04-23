2ee36429f03901687dd1a6562a57dc02f34ffc87
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-23 21:02:03 +0000

    output error fixed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
     body: 'success',
-  })).catch(() => Error()).finally(() => 'Got a response from the API');
+  })).catch(() => Error()).finally(() => console.log('Got a response from the API'));
 }
