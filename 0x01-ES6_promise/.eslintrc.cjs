50554831275bcee7d338286df992b47bafba42b7
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-23 20:49:54 +0000

    task 4 completed

--- 0x01-ES6_promise/.eslintrc.cjs
+++ 0x01-ES6_promise/.eslintrc.cjs
@@ -1,19 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
     },
-    "extends": [
-        "airbnb-base"
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
     },
-    "rules": {
-    }
-};
+    plugins: ['jest'],
+    rules: {
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
+  };
