c5100004b4fc264667e0be5038d17c7279318f98
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-25 20:06:14 +0000

    task 8 completed

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport{
+    constructor(name = '', code = '') {
+        this.name = name;
+        this.code = code;
+      }
+    
+      get [Symbol.toStringTag]() {
+        return `${this.code}`;
+      }
+    
+      get name() {
+        return this._name;
+      }
+    
+      set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+          }
+          this._name = value;
+      }
+    
+      get code() {
+        return this._code;
+      }
+    
+      set code(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('code must be a string');
+          }
+          this._code = value;
+      }
+}
