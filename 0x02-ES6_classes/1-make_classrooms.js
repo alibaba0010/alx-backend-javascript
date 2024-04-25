5e89b9fe0247d6dcf8a88bee88813744c4bb74d5
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-25 18:04:21 +0000

    eslint error task 2 fixed

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
-import ClassRoom from "./0-classroom"
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
-    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
