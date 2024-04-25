f0023cf1bf4966dfee967934e5262b6da1e52c7f
Zakariyah Ali <zakariyahali100@gmail.com>
2024-04-25 05:56:49 +0000

    fixing output error in task 7

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,9 @@
+// Handle multiple promises
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
+
+export default handleProfileSignup;
