17ea161e750de5285e775e2390aeeb972c0b840c
Zakariyah Ali <zakariyahali100@gmail.com>
2024-05-01 08:50:56 +0000

    task_2 dir completed

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,69 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffe break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+function createEmployee(salary: string | number) {
+    if (typeof(salary) === 'number' && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director()
+    }
+}
+
+function isDirector(employee: Teacher | Director): Boolean {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Teacher | Director) {
+    if (employee instanceof Director) {
+        employee.workDirectorTasks();
+    } else if (employee instanceof Teacher) {
+        employee.workTeacherTasks();
+    }
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else {
+        return 'Teaching History';
+    }
+}
